8af737411a2fc147f885d857666e33f51bddf92d
bionara <deathbycornishpastie@hotmail.com>
2016-12-20 21:57:50 +0000

    Initial setup plus basic grid system

---
+++ js/fore.js
@@ -0,0 +1,43 @@
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".top-menu").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".top-menu").removeClass("active");
+    }
+});
+
+$(document).ready(function(){
+	// When the window has finished loading create our google map below
+    google.maps.event.addDomListener(window, 'load', init);
+
+    function init() {
+        // Basic options for a simple Google Map
+        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            zoom: 13,
+
+            // The latitude and longitude to center the map (always required)
+            center: new google.maps.LatLng(13.757496, 100.512867), // Bangkok
+
+            // How you would like to style the map. 
+            // This is where you would paste any style found on Snazzy Maps.
+            styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#b4d4e1"},{"visibility":"on"}]}]
+        };
+
+        // Get the HTML DOM element that will contain your map 
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('map');
+
+        // Create the Google Map using our element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Let's also add a marker while we're at it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(13.757496, 100.512867),
+            map: map,
+            title: 'FORE Management Group'
+        });
+    }	
+});
